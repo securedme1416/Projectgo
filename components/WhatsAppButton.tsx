0c55f7bba5651018ded829aec16642bd6a6b8949
securedme1416 <securepay1416@gmail.com>
2026-02-28 11:54:01 +0100

    Update WhatsAppButton.tsx

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -4,13 +4,10 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 export default function WhatsAppButton() {
   const whatsappNumber = '+2347016211534';
-  const whatsappMessage = encodeURIComponent(
-    'Hello! I would like to get more information about Moni Africa loans.'
-  );
 
   return (
     <a
-      href={`https://wa.me/${whatsappNumber}?text=${whatsappMessage}`}
+      href={`https://wa.me/${whatsappNumber}`}
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white w-16 h-16 rounded-full flex items-center justify-center shadow-lg z-50 transition-transform hover:scale-110"
